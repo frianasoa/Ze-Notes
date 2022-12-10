ef9d705816528aafeac9b5a3c1c73eee5397c9ee
frianasoa <frianasoa@gmail.com>
2022-12-10 20:13:03 +0900

    Getting notes from pdf annotations

--- chrome/content/zenotes/data.js
+++ chrome/content/zenotes/data.js
@@ -71,16 +71,184 @@ Zotero.ZeNotes.data = new function()
         }
         return itemlist;
     }
+   
+    
+    this.creatorshort = function(item)
+    {
+        
+        var creators = item.getCreatorsJSON();
+        var author = "";
+        if(creators.length==1)
+        {
+            author = creators[0].lastName; 
+        }
+        else if(creators.length==2)
+        {
+            author = creators[0].lastName+" and "+creators[1].lastName;
+        }
+        else if(creators.length>0)
+        {
+            author = creators[0].lastName+" et al."
+        }
+        return author;
+    }
+    
+    this.creators = function(item)
+    {
+        var variable = item.getCreatorsJSON();
+        var creators = [];
+        for(i in variable)
+        {
+            var creator = variable[i];
+            creators.push(creator.firstName+" "+creator.lastName);
+        }
+        s = creators.join(", ");
+        return s;
+    }
+    
+    this.year = function(item)
+    {
+        var y = item.getField("date", true).substr(0, 4);
+        return y;
+    }
+    
+    this.filename = function(item)
+    {
+        var filename = "";                    
+        var attachmentIDs = item.getAttachments();
+        if(attachmentIDs.length>0)
+        {
+            var attachment = Zotero.Items.get(attachmentIDs[0]);
+            var path = attachment.attachmentPath;
+            filename = Zotero.Attachments.resolveRelativePath(path);
+        }
+        return filename
+    }
+     
+    this.pdfnotes = function(item)
+    {
+        var notes = [];
+        var ids = item.getAttachments();
+        for(let id of ids)
+        {
+            let attachment = Zotero.Items.get(id);
+            notes = notes.concat(attachment.getAnnotations());
+        }
+        return notes;
+    }
+    
+    this.tags = function(item)
+    {
+        var notes = item.getNotes();
+        var r = {};
+        for(j in notes)
+        {
+            var noteid = notes[j];
+            var n = Zotero.Items.get(noteid);
+            var note = n.getNote();
+            var tag = "";
+            var tags = JSON.parse(JSON.stringify(n)).tags;                
+            
+            if(tags.length>0)
+            {
+                tag = tags[0]["tag"];
+            }
+            
+            if(tag=="")
+            {
+                tag = "Other";
+            }
+            r[tag] = Zotero.ZeNotes.data.clean(note)+ "<span class='notekey'>"+noteid+"</span>";
+        }
+        
+        /** Add pdf tags */
+        
+        var pdfnotes = Zotero.ZeNotes.data.pdfnotes(item);
+        for(j in pdfnotes)
+        {
+            var n = pdfnotes[j];
+            var authors = Zotero.ZeNotes.data.creatorshort(item);
+            var year = Zotero.ZeNotes.data.year(item);
+            var note = n["annotationComment"]+"<h1>Direct quote</h1><div style='background-color:"+n["annotationColor"]+";'>“"+n["annotationText"]+"”</div>("+authors+" "+year+", p. "+n["annotationPageLabel"]+")";
+            
+            var tags = n.getTags();                
+            
+            if(tags.length>0)
+            {
+                tag = tags[0]["tag"];
+            }
+            
+            if(tag=="")
+            {
+                tag = "Other";
+            }
+            r[tag] = Zotero.ZeNotes.data.clean(note)+"<span class='notekey'>"+n["key"]+"</span>";
+        }
+        
+        return r;
+    }
     
     this.tojson = function(items)
     {
         var zenotes = [];
         var taglist = [];
+        
         for(i in items)
         {
-            var item = items[i];            
-            
+            var item = items[i];
+            var tags = Zotero.ZeNotes.data.tags(item);
+            var line = {
+                id: item.getID(),
+                itemid: item.id,
+                key: item.getField("key"),
+                title: item.getField("title"),
+                date: Zotero.ZeNotes.data.year(item),
+                journal: item.getField("publicationTitle"),
+                author: Zotero.ZeNotes.data.creatorshort(item)+" ("+Zotero.ZeNotes.data.year(item)+")",
+                creators: Zotero.ZeNotes.data.creators(item),
+                filename: Zotero.ZeNotes.data.filename(item),
+            }
+            line = Object.assign({},line, tags);
+            if(Object.keys(tags).length>0)
+            {
+                taglist.concat(Object.keys(tags));
+                zenotes.push(line);
+            }
+        }
+        
+        var sorter = Zotero.ZeNotes.settings.lists.sort.map(function(i) {
+            if(i.order=="desc")
+            {
+                return "-"+i.value;
+            }
+            else
+            {
+                return i.value;
+            }
+        });
+        
+        var visibletags = Zotero.ZeNotes.settings.lists.show.map(function(i) {
+            return i.value;
+        });
+        
+        return {
+            columns: visibletags,
+            values: Zotero.ZeNotes.data.sort(zenotes, sorter),
+        }
+    }
+    
+    this.tojson_2 = function(items)
+    {
+        var zenotes = [];
+        var taglist = [];
+        for(i in items)
+        {
+            var item = items[i];
+            var pdfnotes = Zotero.ZeNotes.data.pdfnotes(item);
             var notes = item.getNotes();
+            notes = notes.concat(pdfnotes);
+            console.log(pdfnotes.length);
+            
             if(notes.length>0)
             {
                 try {
@@ -127,8 +295,8 @@ Zotero.ZeNotes.data = new function()
                         title: title,
                         date: date,
                         journal: journal,
-                        author: author,
-                        creators: Zotero.ZeNotes.data.stringify("creators", creators),
+                        author: Zotero.ZeNotes.data.creatorshort(item),
+                        creators: Zotero.ZeNotes.data.creatorshort(item)+" ("+Zotero.ZeNotes.data.year(item)+")",
                         filename: filename,
                     }
                     
@@ -191,18 +359,7 @@ Zotero.ZeNotes.data = new function()
     
     this.stringify = function(mode, variable)
     {
-        var s = ""
-        if(mode=="creators")
-        {
-            var creators = [];
-            for(i in variable)
-            {
-                var creator = variable[i];
-                creators.push(creator.firstName+" "+creator.lastName);
-            }
-            s = creators.join(", ");
-        }
-        return s;
+        
     }
     
     this.clean = function(tdtext)
